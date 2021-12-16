da115287f1e568938215c8681fc0306644c0976b
unknown <alowery299@gmail.com>
2021-12-16 12:14:28 -0600

    updated Spa with add

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+           if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.email = newUser.Email;
+                ViewBag.userName = newUser.UserName;
+                ViewBag.error = "Password must match";
+                return View("Add");
+            }
+        }
+
+    }
+}
